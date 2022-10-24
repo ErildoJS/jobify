9a6a21cf31d001e8fb43c1a47de3644cb1706c66
ErildoJS <patriciopatrick92@gmail.com>
2022-10-24 08:41:15 +0100

    add configs to build

--- index.ts
+++ index.ts
@@ -1,11 +1,17 @@
+import dotenv from 'dotenv'
 import express from 'express'
 import {openDb} from './dbConfig'
 import bodyParser from 'body-parser'
+import path from 'path'
+
+dotenv.config()
+
 const app = express()
 app.use(bodyParser.urlencoded({extended: true}))
 
+app.set('views', path.join(__dirname, 'views'),)
 app.set('view engine', 'ejs')
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')))
 
 const port = process.env.PORT || 3333
 
