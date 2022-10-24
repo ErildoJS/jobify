bfcb18daf7d442a6b7b342fb7902987a05f577c4
ErildoJS <patriciopatrick92@gmail.com>
2022-10-24 18:44:26 +0100

    generate js files

--- dbConfig.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference types="./vendor-typings/sqlite3" />
-import sqlite3 from 'sqlite3';
-export declare function openDb(): Promise<import("sqlite").Database<sqlite3.Database, sqlite3.Statement>>;
